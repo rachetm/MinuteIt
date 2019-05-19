b763871ded783168966089ae3e0f3d376f449f10
Rachet Mudnur <rmudnur@gmail.com>
2019-05-20 01:18:43 +0530

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,22 +1,9 @@
-<!-- <?php
+<?php
 
 	$host = 'localhost';
 	$user = 'root';
 	$password = '';
 	$database = 'minutes';
-	$con = mysqli_connect($host,$user,$password,$database);
-	if(mysqli_connect_error($con))
-	{
-		echo "Failed to connect to MySQL".mysqli_connect_error();
-	}
-?> -->
-
-<?php
-
-	$host = 'localhost';
-	$user = 'id7682031_ise';
-	$password = 'thereugow123';
-	$database = 'id7682031_minuteit';
 	$con = mysqli_connect($host,$user,$password,$database);
 	if(mysqli_connect_error($con))
 	{
