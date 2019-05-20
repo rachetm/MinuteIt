1d227d5eb24467e24816089f1b0c8d61034f53f6
Rachet Mudnur <rmudnur@gmail.com>
2019-05-20 18:13:30 +0530

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,13 +1,10 @@
 <?php
-
-	$host = 'localhost';
-	$user = 'root';
-	$password = '';
-	$database = 'minutes';
-	$con = mysqli_connect($host,$user,$password,$database);
-	if(mysqli_connect_error($con))
-	{
-		echo "Failed to connect to MySQL".mysqli_connect_error();
-	}
+$host = 'remotemysql.com:3306';
+$user = 'ICTaT1CX0N';
+$password = 'jteyvcqTUk';
+$database = 'ICTaT1CX0N';
+$con = mysqli_connect($host, $user, $password, $database);
+if (mysqli_connect_error($con)) {
+	echo "Failed to connect to MySQL" . mysqli_connect_error();
+}
 ?>
-
