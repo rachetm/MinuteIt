1a43bcefa4ce373ee7e2e6722d26d9a2339c91c1
Rachet Mudnur <rmudnur@gmail.com>
2020-01-17 01:50:47 +0530

    New database credentials

--- include/db.php
+++ include/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'remotemysql.com:3306';
-$user = '9QipbsNlGV';
-$password = 'Z0yLEjvRWj';
-$database = '9QipbsNlGV';
+$user = 'KelzoMawf2';
+$password = 'fxzglmvWtH';
+$database = 'KelzoMawf2';
 $con = mysqli_connect($host, $user, $password, $database);
 if (mysqli_connect_error($con)) {
 	echo "Failed to connect to MySQL" . mysqli_connect_error();
